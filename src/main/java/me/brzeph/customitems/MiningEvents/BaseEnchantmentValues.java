9438b9b78420d9f13b3f65b637e3f09456acb55e
BRZeph <ricardocardosodecampos@hotmail.com>
2023-10-31 20:57:35 -0300

    CustomItems-1.0.7-SNAPSHOT WE GOT ENCHANTMENTS BOOOOOOOOOOOOIS

---
+++ src/main/java/me/brzeph/customitems/MiningEvents/BaseEnchantmentValues.java
@@ -0,0 +1,12 @@
+package me.brzeph.customitems.MiningEvents;
+
+public class BaseEnchantmentValues {
+    public static int enchantmentDoubleOre = 0;
+    public static int enchantmentTripleOre = 0;
+    public static int enchantmentMiningSuccess = 0;
+    public static int enchantmentGemFind = 0;
+    public static int enchantmentTreasureFind = 0;
+    public static int enchantmentDurability = 0;
+}
+
+// do not put new base values inside the {} above, it'll clash with the pickaxe generation, specifically on the ''fields'' line
---
+++ src/main/java/me/brzeph/customitems/MiningEvents/RandomValueGenerators.java
@@ -0,0 +1,38 @@
+package me.brzeph.customitems.MiningEvents;
+
+import java.util.Random;
+
+public class RandomValueGenerators {
+    public static int doubleOreRandomRoll(){
+        Random random = new Random();
+
+        return random.nextInt(100) + 1;
+    }
+    public static int tripleOreRandomRoll(){
+        Random random = new Random();
+
+        return random.nextInt(100) + 1;
+    }
+    public static int gemFindRandomRollForEnchantment(){
+        Random random = new Random();
+        return random.nextInt(100) + 1;
+    }
+    public static int gemFindRandomRollForGems(){
+        Random random = new Random();
+        int minValue = 30;
+        int maxValue = 201;
+
+        return random.nextInt(maxValue - minValue + 1) + minValue;
+    }
+    public static int treasureFindRandomRoll(){
+        Random random = new Random();
+        return random.nextInt(100) + 1;
+    }
+    public static int t1OreXPGenerator(){
+        Random random = new Random();
+        int minValue = 80;
+        int maxValue = 150;
+
+        return random.nextInt(maxValue - minValue + 1) + minValue;
+    }
+}
